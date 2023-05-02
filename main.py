40dd6b23dda837753ee089f5289dac856d7a7a90
adriank64216 <sqat1024@gmail.com>
2023-05-02 22:39:37 +0200

    checking is word correct from txt file

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ def generateWord(letters):
     return permutations(letters)
  
 words = generateWord(['f', 'i'])
-# Print the obtained permutations
-for i in list(words):
-    print (i)
+
+f = open('odm.txt', encoding="utf8")
+
+def isWordCorrect(word):
+    while True:
+        line = f.readline()
+        if line != '':
+            line = line.replace(' ', '')
+            line = line.replace('\n', '')
+            line = line.split(',')
+            if word in line:
+                return True
+        else:
+            return False
+        
+print(isWordCorrect('zez'))
